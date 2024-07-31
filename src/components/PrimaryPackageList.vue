dae58da357a33406e6fe18eb745139abbb360942
BaiYanchen <2786660867@qq.com>
2024-07-31 11:34:54 +0800

    feat: ✨ PrimaryPackageList添加物logo时显示首先字母

--- src/components/PrimaryPackageList.vue
+++ src/components/PrimaryPackageList.vue
@@ -9,15 +9,20 @@ defineProps<{
 
 export interface List {
   name: string
-  logo: string
+  logo?: string
 }
 </script>
 
 <template>
   <ul min-w-140px flex flex-col gap-10px rounded-md>
     <li v-for="item in list" :key="item.name" flex items-center overflow-hidden rounded-md text-14px font-600 bg="#1B1B1F">
-      <div size-24px flex p-6px bg="#161618">
-        <img w-full :src="item.logo">
+      <div bg="#161618" text="[#41B883]" size-24px flex items-center justify-center p-6px text-20px>
+        <template v-if="item.logo">
+          <img w-full :src="item.logo">
+        </template>
+        <template v-else>
+          {{ item.name.substring(0, 1) }}
+        </template>
       </div>
       <div px-10px>
         {{ item.name }}
